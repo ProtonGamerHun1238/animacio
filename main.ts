7f8ba354946b4997bc4008183f8cc02f294d09c2
ProtonGamerHun1238 <68368558+ProtonGamerHun1238@users.noreply.github.com>
2020-07-16 09:00:32 +0200

    yessir

--- main.ts
+++ main.ts
@@ -1,6 +1,37 @@
 input.onButtonPressed(Button.A, function () {
     while (asd == 1) {
         basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            # . # . #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
+input.onButtonPressed(Button.B, function () {
+    while (asd == 1) {
+        basic.showLeds(`
             . # . # .
             # # # # #
             # # # # #
@@ -10,19 +41,17 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(500)
         basic.showLeds(`
             . . . . .
-            . # . # .
-            . # # # .
-            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
             . . . . .
             `)
         basic.pause(500)
     }
 })
-input.onButtonPressed(Button.AB, function () {
-    control.reset()
-})
 let asd = 0
 asd = 1
+basic.showString("Hello!")
 basic.forever(function () {
 	
 })
