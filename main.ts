f27ae342b1205efcd7d487b969ef9be786fdb371
ProtonGamerHun1238 <68368558+ProtonGamerHun1238@users.noreply.github.com>
2020-07-16 08:52:37 +0200

    yessir

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    while (asd == 1) {
+        basic.showLeds(`
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        basic.pause(500)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
+let asd = 0
+asd = 1
 basic.forever(function () {
 	
 })
